387b4946d5ab1e712dd818e180e1b1cf137fe2ac
Amadu <amaduhamzajaarah300@gmail.com>
2025-07-29 11:05:05 -1200

    created index

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -1,27 +1,40 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+    return (
+        <View
+            style={styles.container}
+        >
+            <Text>Entry Screen - Awesome</Text>
+            <View>
+                <Text style={styles.largeText}>Typescript is great if you practice more</Text>
+                <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
+                <Text style={styles.smallText}>ALX is awesome</Text>
+            </View>
+        </View>
+    );
+}
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "#90caf9",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 20,
     },
     largeText: {
-        fontSize: 30,
-        color: "#f44336",
-        marginBottom: 5,
-        fontWeight: "700",
-        fontVariant: ["small-caps"],
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 10,
+        textAlign: "center",
     },
     mediumText: {
-        fontSize: 20,
-        color: "#9c27b0",
+        fontSize: 18,
         marginBottom: 10,
-        fontWeight: "500",
-        textAlign: "right",
+        textAlign: "center",
     },
     smallText: {
-        fontSize: 15,
-        color: "#2196f3",
-        fontWeight: "400",
+        fontSize: 14,
         textAlign: "center",
     },
 });
